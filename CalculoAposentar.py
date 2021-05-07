eea99c212591810c4e89ed3cb50cd0eca4878f1f
BrunoBin <35066292+Brunobtt@users.noreply.github.com>
2021-05-07 14:55:01 -0300

    Update CalculoAposentar.py

--- CalculoAposentar.py
+++ CalculoAposentar.py
@@ -1,25 +1,29 @@
 # Codigo do Bin para identificar tempo restante para se aposentar
 
-
-def se_mulher(idade_mulher): #funcao para calcular o tempo restante se for mulher
+#funcao para calcular o tempo restante se for mulher
+def se_mulher(idade_mulher): 
   mulher_idade_minina = 62
   restante = mulher_idade_minina - idade_mulher
   mostrar_tempo(restante)
 
-def se_homem(idade_homem): #funcao para calcular o tempo restante se for homem
+#funcao para calcular o tempo restante se for homem
+def se_homem(idade_homem): 
   homem_idade_minima = 65
   restante = homem_idade_minima - idade_homem
   mostrar_tempo(restante)
 
-def mostrar_tempo(resta): #funcao para mostrar o tempo restante
+#funcao para mostrar o tempo restante  
+def mostrar_tempo(resta): 
   print('Ainda faltam', resta, 'anos para voce se aposentar!')
 
 print ('Ola, vamos descobrir quanto tempo ainda falta para voce se aposentar, para isso precisamos de algumas informacoes:')
 
-idade = int(input('Sua idade:')) #Coletando idade da pessoa
+#Coletando idade da pessoa
+idade = int(input('Sua idade:')) 
 sexo = str(input('Agora nos infome seu sexo, se for homem responda "H", se for mulher responda "M":')) #Coletando sexo da pessoa
 
+#Se for mulher chama a funcao se_mulher, Se for homem chama a funcao se_homem
 if sexo == 'M': 
-  se_mulher(idade) #Se for mulher chama a funcao se_mulher
+  se_mulher(idade) 
 else: 
-  se_homem(idade) #Se for homem chama a funcao se_homem
+  se_homem(idade) 
